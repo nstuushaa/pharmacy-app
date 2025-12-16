744ce283e2fd7ea972ae81253db9affc5d812e23
nstuushaa <nnastyy06@yandex.ru>
2025-12-16 17:29:56 +0300

    исправления: переход на страницу профиля

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function index(){
-        return view('favourite');
-    }
+    public function index()
+    {
+        return view('profile');  
+}
 }
