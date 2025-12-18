900ab81bacbb659f1c7fe413ac8b2f0d539309f0
nstuushaa <nnastyy06@yandex.ru>
2025-12-18 14:12:23 +0300

    добавление: начало создание секции с банерами

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use Illuminate\Http\Request;
 use App\Models\City;
 
 class HomeController extends Controller
 {
     public function index()
-{
-
-}
+    {
+        $banners = Banner::all();
+        return view('home', compact('banners'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,14 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\FavouriteController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\DeliveryPageController;
+use App\Http\Controllers\HomeController;
 
 //Главная страница
-Route::get('/', function () {
-    return view('layouts.app');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 Route::get('/favourite', [FavouriteController::class, 'index'])->name('favourite');
 Route::get('/catalog/{slug}', [CatalogController::class, 'show'])->name('catalog.show');
+Route::get('/delivery-info', [DeliveryPageController::class, 'index'])->name('delivery-info');
 
